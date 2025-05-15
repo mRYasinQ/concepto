ba41d3c1db603a1fa9e2f196e89a6dcd62b505e6
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2025-05-15 11:41:12 +0330

    fix: change bottom to 10%

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import { HeroImages } from './HeroImages';
 
 export const Hero = () => {
     return (
-        <section className="before:bg-primary after:bg-primary relative container flex flex-col items-center justify-center gap-8 py-10 before:absolute before:top-0 before:left-[98%] before:z-[-1] before:hidden before:size-40 before:blur-[450px] before:content-[''] after:absolute after:right-[98%] after:bottom-0 after:z-[-1] after:hidden after:size-40 after:blur-[450px] after:content-[''] lg:py-24 lg:before:block lg:after:block">
+        <section className="before:bg-primary after:bg-primary relative container flex flex-col items-center justify-center gap-8 py-10 before:absolute before:top-0 before:left-[98%] before:z-[-1] before:hidden before:size-40 before:blur-[450px] before:content-[''] after:absolute after:right-[98%] after:bottom-[10%] after:z-[-1] after:hidden after:size-40 after:blur-[450px] after:content-[''] lg:py-24 lg:before:block lg:after:block">
             <div className="flex flex-col gap-4 text-center lg:gap-6">
                 <TypeAnimation
                     className="text-primary-shade-700 lg:display-2 text-[20px] font-bold"
