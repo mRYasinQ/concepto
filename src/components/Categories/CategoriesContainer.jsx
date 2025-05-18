08f034f30fbf2a7bf49d433238701b970a419b39
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2025-05-18 11:56:12 +0330

    fix: update categories component for responsive

--- src/components/Categories/CategoriesContainer.jsx
+++ src/components/Categories/CategoriesContainer.jsx
@@ -4,7 +4,7 @@ import CategoryItems from '../../constants/CategoryItems';
 
 export const CategoriesContainer = ({}) => {
     return (
-        <div className="grid grid-cols-4 gap-y-6 md:grid-cols-5 lg:grid-cols-7">
+        <div className="grid grid-cols-[repeat(4,auto)] justify-between gap-y-6 md:grid-cols-[repeat(5,auto)] lg:grid-cols-[repeat(7,auto)]">
             {CategoryItems.map((category, index) => (
                 <Category key={`${category.name}-${index}`} categoryData={category} />
             ))}
--- src/components/Categories/Category.jsx
+++ src/components/Categories/Category.jsx
@@ -1,6 +1,6 @@
 export const Category = ({ categoryData: { name, link, image } }) => {
     return (
-        <a href={link} className="m-auto flex h-21.5 w-14 flex-col items-center gap-2 md:h-[141px] md:w-24 md:gap-4">
+        <a href={link} className="flex h-21.5 w-14 flex-col items-center gap-2 md:h-[141px] md:w-24 md:gap-4">
             <div className="border-primary rounded-full md:border-2 md:p-1.5">
                 <img src={`/images/categories/${image}`} alt={name} />
             </div>
