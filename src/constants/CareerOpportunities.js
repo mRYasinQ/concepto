c99bfc924514adf1f7b35eb3a048b528c5cc79b5
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2025-05-20 00:32:52 +0330

    chore: compress images

--- src/constants/CareerOpportunities.js
+++ src/constants/CareerOpportunities.js
@@ -1,8 +1,8 @@
 const CareerOpportunities = [
-    { company: { logo: 'bluebank.png', name: 'بلو بانک' }, opportunity: 12 },
-    { company: { logo: 'asiatech.png', name: 'آسیاتک' }, opportunity: 16 },
+    { company: { logo: 'bluebank.jpg', name: 'بلو بانک' }, opportunity: 12 },
+    { company: { logo: 'asiatech.jpg', name: 'آسیاتک' }, opportunity: 16 },
     { company: { logo: 'farabi.png', name: 'کارگزاری فارابی' }, opportunity: 11 },
-    { company: { logo: 'bimebazar.png', name: 'بیمه بازار' }, opportunity: 14 },
+    { company: { logo: 'bimebazar.jpg', name: 'بیمه بازار' }, opportunity: 14 },
     { company: { logo: 'alibabagroup.png', name: 'گروه علی بابا' }, opportunity: 18 },
 ];
 
