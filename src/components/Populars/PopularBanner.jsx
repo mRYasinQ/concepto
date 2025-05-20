99f11df29d9ac9fb55e2ed5dccfc455c294cf61d
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2025-05-20 08:39:03 +0330

    fix: update mask-image paths

--- src/components/Populars/PopularBanner.jsx
+++ src/components/Populars/PopularBanner.jsx
@@ -1,6 +1,6 @@
 export const PopularBanner = ({ company: { name, banner } }) => {
     return (
-        <div className="h-33.5 overflow-hidden rounded-t-3xl mask-[image:url('images/layouts/popular.svg')] mask-no-repeat">
+        <div className="h-33.5 overflow-hidden rounded-t-3xl mask-[image:url('/images/masks/popular.svg')] mask-no-repeat">
             <img className="h-full w-full" src={`images/companies/banner/${banner}`} alt={name} />
         </div>
     );
